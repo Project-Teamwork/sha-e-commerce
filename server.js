57bed31c84227601578169951b7ba6e7170ee1a1
Anita Angelidou <anitawebdesigns@gmail.com>
2023-01-20 21:27:59 +0200

    completed registerUser and loginUser in the backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const bodyparser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 3636;
+const cors = require('cors');
+const authRouter = require('./routers/authRoute');
+const User = require('./modules/userModule');
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.use("/users", authRouter)
+
+
+
+// server listening
+app.listen(port, () => {
+  console.log(`Server is up at ${port}`);
+});

2c9d124f8558d8071770518977e5ba77b2f329d3
Anita Angelidou <anitawebdesigns@gmail.com>
2023-01-21 20:42:03 +0200

    created ui and css of register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,90 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url("https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80");
+  color: white;
+`;
+
+const Title = styled.h1 `
+margin: 0;
+padding-block: 1rem 0;
+padding-inline: 1rem;
+text-align: left
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin: 0.4rem 0;
+`;
+
+const Input = styled.input`
+  width: 15rem;
+  height: 20px;
+  margin-block: 0.4rem;
+  font-size: 1.1rem;
+`;
+
+const Navbar = styled.div `
+display: flex;
+justify-content: space-between;
+`
+
+const Button = styled.button `
+width: 10rem;
+ margin-block: 0.5rem 0;
+ margin-inline: 2rem;
+ font-size: 1.2rem;
+ background-color:#DC7300;
+ border-radius: 5px;
+ transition: 0s background-color;
+ 
+ cursor: pointer;
+ &:hover {
+  background-color:#E05404;
+  transition-delay: 0.1s;
+ }
+ &:active {
+  background-color:#7F1917;
+ }
+
+`
+
 function Register() {
-  return <div>Register</div>;
+  return (
+    <Container>
+        <Navbar>
+          <Title>Register</Title>
+          <Button>Create Account</Button>
+        </Navbar>
+      <Wrapper>
+        <Label>
+          Email:
+          <Input />
+        </Label>
+        <Label>
+          Username:
+          <Input />
+        </Label>
+        <Label>
+          Password:
+          <Input />
+        </Label>
+      </Wrapper>
+    </Container>
+  );
 }
 
 export default Register;
